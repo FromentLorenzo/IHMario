ce42d8e2c05d84d318bb519742603b852b0858eb
Lorenzo Froment <lorenzo.froment@etu.unice.fr>
2024-03-27 17:00:39 +0100

    dialogues

--- Assets/PlayerInteract.cs
+++ Assets/PlayerInteract.cs
@@ -1,23 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
-//using System.Diagnostics;
 using UnityEngine;
 
 public class PlayerInteract : MonoBehaviour
 {
+	
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
             float interactRange = 3f;
             Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
-            foreach(Collider collider in colliderArray)
+            foreach (Collider collider in colliderArray)
             {
-                if(collider.TryGetComponent(out NPCInterract npcinterract))
+                if (collider.TryGetComponent(out NPCInterract npcinterract))
                 {
                     npcinterract.Interract();
+                }
+
+                if (collider.TryGetComponent(out NPCInterractMicroscope microscopeInterract))
+                {
+                    microscopeInterract.Interract();
                 }
             }
         }
     }
+	
 }
